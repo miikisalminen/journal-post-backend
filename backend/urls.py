0ade00f22e4f8643c7461cefa5eba5cd5585e886
spooki <miika.salminen11@outlook.com>
2023-12-27 11:30:13 +0200

    Updated file structure

--- backend/urls.py
+++ backend/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("backend.journalpost.urls"))
+    path('', include("journalpost.urls"))
 ]
 
