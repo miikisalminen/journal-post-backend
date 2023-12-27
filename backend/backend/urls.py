60f3a4b962c7e45f7d0cede3961f11cdad3016e3
spooki <miika.salminen11@outlook.com>
2023-12-27 11:20:00 +0200

    2

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("journalpost.urls"))
+    path('', include("backend.journalpost.urls"))
 ]
 
